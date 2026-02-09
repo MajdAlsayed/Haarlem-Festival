d4fcb220f40e4be757ed5d4b015692b732588c59
NatDobin <715547@student.inholland.nl>
2026-02-09 18:22:23 +0100

    Remove custom migration system

--- database/migrations/002_create_roles.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE roles (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       role_name VARCHAR(50) UNIQUE NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO roles (role_name) VALUES
-                                  ('customer'),
-                                  ('admin'),
-                                  ('visitor');
